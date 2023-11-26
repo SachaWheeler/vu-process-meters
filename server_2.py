aa2404cebda54215114285aa1595c937158a6339
Sacha Wheeler <sacha@sachawheeler.com>
2023-11-26 09:15:49 +0000

    remove debugging

--- server_2.py
+++ server_2.py
@@ -12,11 +12,11 @@ ser = serial.Serial(TTY_PORT, BAUDRATE)
 # Function to send values to Arduino
 def send_values(value1, value2):
     data = f"{value1},{value2}\n"
-    print(data.strip())
+    # print(data.strip())
     ser.write(data.encode())
     sleep(0.1)  # Allow time for Arduino to process
 
-# Example: Send values 42 and 78
+print("Server running...")
 while True:
     ram=float(psutil.virtual_memory().percent)
     cpu=float(psutil.cpu_percent())
@@ -27,28 +27,3 @@ while True:
 # Close the serial connection
 ser.close()
 
-
-"""
-arduino = serial.Serial(port=TTY_PORT, baudrate=BAUDRATE, timeout=.1)
-
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.1)
-    resp = arduino.readline()
-    return resp
-
-for n in range(1,255,10):
-    print(n)
-    print(write_read(f"{n},{n}\r"))
-    time.sleep(1)
-# 255 and 2.55 are veruy uncertain
-while True:
-    break
-    ram=int(psutil.virtual_memory().percent)
-    cpu=int(psutil.cpu_percent())
-    response = write_read(f"{ram},{cpu}\r")
-    print(ram, cpu, response)
-
-    sleep(0.5)
-    # break
-"""
